3923d47ae959a238cd85359d958a6dfe57d823b6
hiroaki Ohhashi <naruhiron@gmail.com>
2018-09-04 01:06:40 +0900

    added string concateration

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -8,6 +8,20 @@ import (
 	"github.com/NAL-6295/monkey/parser"
 )
 
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
 func TestStringLiteral(t *testing.T) {
 	input := `"Hello World!"`
 
@@ -131,6 +145,10 @@ if (10 > 1){
 		{
 			"foobar",
 			"identifier not found: foobar",
+		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
 		},
 	}
 
